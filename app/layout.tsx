791cb786f8fd73cccbeabf31f1c07e166abab3ab
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2023-10-03 13:36:57 +0200

    feat(navbar)implement navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,31 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import {UserProvider} from '@auth0/nextjs-auth0/client'
-import AuthProvider from '@/components/authProvider/authProvider'
+import './globals.css';
+import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
+import {UserProvider} from '@auth0/nextjs-auth0/client';
+import AuthProvider from '@/components/authProvider/authProvider';
+import {Navbar} from '@/components/navbar/navbar';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
-  title: 'MovieHub',
-  description: 'Next JS site for your movies',
-  icons: {
-    icon:"/31087.png"
- }
-}
+	title: 'MovieHub',
+	description: 'Next JS site for your movies',
+	icons: {
+		icon: '/31087.png',
+	},
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <UserProvider>
-          <AuthProvider>
-        {children}
-        </AuthProvider>
-        </UserProvider>
-      </body>
-    </html>
-  )
+export default function RootLayout({children}: {children: React.ReactNode}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<UserProvider>
+					<AuthProvider>
+						<Navbar />
+						{children}
+					</AuthProvider>
+				</UserProvider>
+			</body>
+		</html>
+	);
 }
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+'use client'
+import styles from './navbar.module.css'
+import {useUser} from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
 
-type Props = {}
+export const Navbar = () => {
+	const { user, error, isLoading} = useUser();
 
-const Navbar = (props: Props) => {
-  return (
-    <div>navbar</div>
-  )
-}
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
 
-export default Navbar
+  if (error) {
+    return <div>{error.message}</div>
+  }
+
+	return (
+		<div className={styles.navbar}>
+			<div className={styles.navbar__links}>
+				<div className={styles.navbar__links__left}>
+					{user ? (<Link href='/private'>MOVIE'S TIME</Link>) : (<Link href='/'>MOVIE'S TIME</Link>)}
+				</div>
+				{user ? (<h3 className={styles.navbar__links__right__welcome}>Welcome {user?.name}</h3>) : (<h3 className="styles.navbar__links-right__welcome">Please log in</h3>)}
+				<div className={styles.navbar__links__right}>
+					{user ? (
+						<>
+							{/* <ModalCreationMovies getToken={() => getAccessTokenSilently()} /> */}
+							<div className={styles.navbar__divButtonProfile}>
+								<Link href='/private/profile' className={styles.profile__link}>
+									Profile
+								</Link>
+							</div>
+							<a href="/api/auth/logout">Logout</a>
+						</>
+					) : (
+						<a href={'/api/auth/login'}>Login</a>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
